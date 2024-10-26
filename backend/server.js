e932945462da59ec5a3e8436e02a6199b1534cba
Modassir Imran <96190246+Modassir-Imran@users.noreply.github.com>
2024-10-26 17:26:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 6000
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://food-order-app-frontend-hfku.onrender.com");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
