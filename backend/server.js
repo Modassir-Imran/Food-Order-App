666c0c6f11bc461ca857fcaccc28ce15882efa57
Modassir Imran <96190246+Modassir-Imran@users.noreply.github.com>
2024-10-26 15:20:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 6000
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3002");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
